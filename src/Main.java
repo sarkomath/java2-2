4edd727df0ece90f048215a2ecfaff3f9a604e85
Nikita Novoselov <sarkomath@outlook.com>
2020-11-05 20:41:01 +1000

    add program

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double regularBonus = 0.7;
+        double specialBonus = 0.2;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
