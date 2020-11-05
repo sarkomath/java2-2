8a7bb3cd88d89fb0982f419e7a48dc389690be4c
Nikita Novoselov <sarkomath@outlook.com>
2020-11-05 21:16:28 +1000

    report compl.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double regularBonus = 0.7;
-        double specialBonus = 0.2;
+        double regularBonus = 0.6;
+        double specialBonus = 0.3;
         double totalBonus = regularBonus + specialBonus;
         System.out.println(totalBonus);
     }
